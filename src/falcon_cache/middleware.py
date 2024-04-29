1f7670bdb1be6fe587bd81ce5b4b126fb29852a3
Georgiy <metallerok@gmail.com>
2024-04-29 12:28:29 +0300

    init

---
+++ src/falcon_cache/middleware.py
@@ -0,0 +1,15 @@
+from src.falcon_cache.cache import APICache
+from redis import Redis
+
+
+class CacheMiddleware:
+    @classmethod
+    def process_response(cls, req, resp, resource, req_succeeded):
+        if not req_succeeded:
+            return
+
+        redis: Redis = req.context["redis"]
+        key = APICache.make_cache_key(req)
+
+        if req.method in APICache.invalidate_methods:
+            redis.delete(key)
